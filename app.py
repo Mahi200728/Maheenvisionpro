e632464749d223ee60faa40d54a47d9ca61ba697
Javeria Maheen <156617109+Mahi200728@users.noreply.github.com>
2024-12-31 16:55:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 image = ""   
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.", use_container_width=True)
 
 # Button variables
 tell_me_button = st.button("Tell me about the image")
